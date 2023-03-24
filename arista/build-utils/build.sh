9be410f0928e9683f6257a5a0ea5700f0a5bdd0b
Satish Kumar Eerpini <seerpin@arista.com>
2023-03-24 12:04:40 -0700

    Upgrade to Aqua SAI 9.0.0.7 EA drop.
    - Upgrade to new SAI implementation drop.
    - Upgrade to new SDK drop.
    - Add patch to fix SDK DNX Kernel module builds.
    - Update internal build directory structure change for new SAI drop.

--- arista/build-utils/build.sh
+++ arista/build-utils/build.sh
@@ -93,9 +93,9 @@ pip3 install GitPython
 SAI_DIR="$FBOSS_DIR/Aqua_SAI/"
 if [ $ARCH == "dnx" ];
 then
-   SAI_BUILD_DIR="$SAI_DIR/output/x86-dnx-deb-static-meta"
+   SAI_BUILD_DIR="$SAI_DIR/output/x86-dnx-deb-static-fboss"
 else
-   SAI_BUILD_DIR="$SAI_DIR/output/x86-xgsall-deb-static"
+   SAI_BUILD_DIR="$SAI_DIR/output/x86-xgsall-deb-static-fboss"
 fi
 
 # Delete SDK var in the env. It can cause the SDK build to fail.
