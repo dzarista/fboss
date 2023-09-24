4cb1822853c1f416ac7055cdddc95f3898f033c5
Satish Kumar Eerpini <seerpini@arista.com>
2023-09-24 15:52:57 -0700

    Logical port IDs for fabric ports should match logical lane numbers
    - Since the SDK logical port IDs for Fabric ports are automatically
      assigned by the SDK based on lane maps (the logical lane map number is
      the logical port ID), we want to use the same number for port profile
      mapping's logical port ID as well.
    - The logical port ID from port profile mapping is used to generate the
      SW port id in FBOSS' platform mapping.

--- arista/platform/meru800bfa/scripts/gen_whistler_platform_mapping.py
+++ arista/platform/meru800bfa/scripts/gen_whistler_platform_mapping.py
@@ -389,22 +389,20 @@ with open( "whistler_port_profile_mapping.csv", "w" ) as fh:
    # NOTE : For Fabric ports, there is no core binding, the corresponding
    # Attached_CoreId and Attached_Core_PortID can be left empty.
    fabSupportedProfiles = '-'.join( numLanesFromSupportedProfile.keys() )
-   asicLogicalPortId = {}
-   asicGlobalPortId = {}
    # FBOSS assigns a range of 2k ports to each NPU, we will only use the first 512
    # IDs from this space.
    fabricPortsPerAsic = 2048
-   for asicId in range( numAsics ):
-      asicLogicalPortId[ asicId ] = 0
-      asicGlobalPortId[ asicId ] = ( fabricPortsPerAsic * asicId )
    for portId in range( numFabricPorts ):
       frontPanelSlot  = ( portId // 8 ) + 1
       frontPanelLane = portId % 8 + 1
       frontPanelPortStrKey = f"{frontPanelSlot}/{frontPanelLane}"
       chipId = frontPanelToAsicSerdesMap[ frontPanelPortStrKey ][ "chipId" ]
+      rxPhysicalLane = frontPanelToAsicSerdesMap[ frontPanelPortStrKey ][ "rx" ]
+      logicalLane = asicSerdesToLogicalLaneMap[ chipId ][ "rx" ][ rxPhysicalLane ]
       portStr = f"fab1/{frontPanelPortStrKey}"
-      fh.write( f"{asicGlobalPortId[chipId]},{asicLogicalPortId[chipId]},{portStr},{fabSupportedProfiles},,\n" )
-      asicLogicalPortId[ chipId ] += 1
-      asicGlobalPortId[ chipId ] += 1
+      logicalPortId = logicalLane
+      assert logicalLane < fabricPortsPerAsic
+      globalPortId = ( fabricPortsPerAsic * chipId ) + logicalPortId
+      fh.write( f"{globalPortId},{logicalPortId},{portStr},{fabSupportedProfiles},,\n" )
 
 bcmConfigFh.close()
