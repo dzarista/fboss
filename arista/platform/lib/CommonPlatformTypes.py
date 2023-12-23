5bc63c377c9772e04920c24d6cebca8bab13d35c
Satish Kumar Eerpini <seerpin@arista.com>
2023-12-22 22:23:14 -0800

    Helpers for speed in mbps and valid media for speed.

--- arista/platform/lib/CommonPlatformTypes.py
+++ arista/platform/lib/CommonPlatformTypes.py
@@ -2,14 +2,39 @@
 # Arista Networks, Inc. Confidential and Proprietary.
 from enum import Enum
 
-PortMedium = Enum( 'PortMedium', 'Copper Optical' )
+PortMedium = Enum( 'PortMedium', 'OPTICAL COPPER' )
 SpeedGbps = Enum( 'SpeedGbps',
       'TwentyFive Fifty FiftyThree Hundred HundredAndSix FourHundred' )
 
-def validNifSerdesSpeeds():
-   return ( SpeedGbps.Fifty, SpeedGbps.Hundred )
+def speedInMbps( speed: SpeedGbps ) -> int:
+   if speed == SpeedGbps.TwentyFive:
+      return 25000
+   elif speed == SpeedGbps.Fifty:
+      return 50000
+   elif speed == SpeedGbps.FiftyThree:
+      return 53125
+   elif speed == SpeedGbps.Hundred:
+      return 100000
+   elif speed == SpeedGbps.HundredAndSix:
+      return 106250
+   elif speed == SpeedGbps.FourHundred:
+      return 400000
+   else:
+      assert False, f"Invalid speed {speed}"
 
-def validFabricSerdesSpeeds():
+
+def validNifSerdesSpeeds() -> tuple[SpeedGbps]:
+   return ( SpeedGbps.Hundred, SpeedGbps.Fifty, SpeedGbps.TwentyFive )
+
+def validFabricSerdesSpeeds() -> tuple[SpeedGbps]:
    return ( SpeedGbps.FiftyThree, SpeedGbps.HundredAndSix )
 
-
+def validMediaForSpeed( speed: SpeedGbps ) -> tuple[PortMedium]:
+   if speed in ( SpeedGbps.Hundred, SpeedGbps.TwentyFive, SpeedGbps.HundredAndSix ):
+      return PortMedium.__members__.values()
+   elif speed == SpeedGbps.Fifty:
+      return ( PortMedium.COPPER, )
+   elif speed == SpeedGbps.FiftyThree:
+      return tuple()
+   else:
+      assert False, f"Invalid speed {speed}"
