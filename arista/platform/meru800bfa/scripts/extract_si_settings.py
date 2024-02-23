1c3f9d428051e5ff24f2298329f9595718e25945
Satish Kumar Eerpini <seerpini@arista.com>
2024-02-22 21:25:01 -0800

    s/Whistler_SI_Settings.csv/Whistler_Hw_SI_Settings_v3.csv

--- arista/platform/meru800bfa/scripts/extract_si_settings.py
+++ arista/platform/meru800bfa/scripts/extract_si_settings.py
@@ -6,12 +6,12 @@ Extract the relevant fields to map SI settings from
 Whistler_port_mapping_SI_settings_v3.csv, which itself is a csv file from
 https://docs.google.com/spreadsheets/d/1_IAStZOz2Xc7oQuB0oRmaBqd-IlNKUEh13mVHr6wkMQ/edit#gid=1100430376
 
-Output of this script is Whistler_SI_settings.csv, which can then be used by the
+Output of this script is Whistler_Hw_SI_Settings_v3.csv, which can then be used by the
 gen_*vendor_mapping script to update SI settings in the vendor mapping sheet.
 """
 
 with open( "Whistler_port_mapping_SI_settings_v3.csv" ) as fh, open(
-   "Whistler_SI_Settings.csv", "w") as siFh:
+   "Whistler_Hw_SI_Settings_v3.csv", "w") as siFh:
    reader = csv.reader( fh )
    writer = csv.writer(siFh, lineterminator='\n', quoting=csv.QUOTE_NONE)
    header = [ "OSFP Port", "OSFP Lane", "Pre3", "Pre2", "Pre", "Main", "Post",
