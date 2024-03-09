b4eb9635329a794e9069ec2ac9d364170d9a6d72
Satish Kumar Eerpini <seerpini@arista.com>
2024-03-08 16:17:45 -0800

    Always force platform_manager to reload BSP kmods on init.

--- arista/core/scripts/run_platform_manager.sh
+++ arista/core/scripts/run_platform_manager.sh
@@ -7,7 +7,7 @@ BIN=/opt/fboss/bin/platform_manager
 CONF=/opt/fboss/share/platform_configs/platform_manager.json
 # FIXME: BUG912963 reduce the interval size once PM has been optimized for running as a deamon.
 # It currently floods the console with logs everytime it reruns after the first time.
-ARGS=(-config-file "${CONF}" -noenable_pkg_mgmnt -explore_interval_s 3600)
+ARGS=(-config-file "${CONF}" -noenable_pkg_mgmnt -explore_interval_s 3600 -reload_kmods=true)
 
 PROCESS="$BIN ${ARGS[@]}"
 
