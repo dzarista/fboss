d1f0a336525c72c88a6aaf0bd19b0ace131899ae
aalamsi22 <alamsi@arista.com>
2024-03-26 16:08:49 -0700

    remove whitespace

--- arista/bsp-kmods/scd-leds.c
+++ arista/bsp-kmods/scd-leds.c
@@ -212,7 +212,7 @@ static int scd_led_probe(struct auxiliary_device *auxdev,
 		sprintf(led_name, "%s_port%d_led%d", pdata->id.name, led_data.port_num,
 			 led_data.led_idx);
 	else sprintf(led_name, "%s%d", pdata->id.name, led_data.led_idx);
-	
+
 	ret = scd_leds_init(priv, led_name);
 
 	if (ret)
