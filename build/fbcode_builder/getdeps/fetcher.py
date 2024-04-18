6b52b10326056b7f161ffea1fa7071c2a10e91ae
Cody Ohlsen <codyohl@meta.com>
2024-04-17 17:24:31 -0700

    getdeps: support GETDEPS_WGET_ARGS in wget version, support skipping lfs/upload steps
    Summary:
    X-link: https://github.com/facebookincubator/katran/pull/229
    
    X-link: https://github.com/facebookexperimental/edencommon/pull/18
    
    X-link: https://github.com/facebook/sapling/pull/878
    
    X-link: https://github.com/facebook/openr/pull/154
    
    X-link: https://github.com/facebook/folly/pull/2177
    
    X-link: https://github.com/facebookincubator/zstrong/pull/748
    
    I found it useful to be able to set `GETDEPS_WGET_ARGS` to change some of the flags to `wget` while it's in that fetch mode :)
    I also need to skip the lfs upload part in my environment
    
    Reviewed By: mackorone
    
    Differential Revision: D56263907
    
    fbshipit-source-id: ae45c31ebb10123e0358544d294fe2f2979dd59a

--- build/fbcode_builder/getdeps/fetcher.py
+++ build/fbcode_builder/getdeps/fetcher.py
@@ -694,7 +694,7 @@ def download_url_to_file_with_progress(url: str, file_name) -> None:
                     url,
                 ]
             )
-            subprocess.run(procargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            subprocess.run(procargs, capture_output=True)
             headers = None
 
         elif os.environ.get("GETDEPS_USE_LIBCURL") is not None:
