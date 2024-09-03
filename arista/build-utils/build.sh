79959782fa02afeef0c4b56238f4e4e949994b1a
Satish Kumar Eerpini <seerpini@arista.com>
2024-09-03 11:09:34 -0700

    Generate py thrift bindings for PHY and HW_CTRL FBOSS interfaces

--- arista/build-utils/build.sh
+++ arista/build-utils/build.sh
@@ -344,6 +344,8 @@ else
 
    # Generate python thrift libraries
    $SCRATCH_DIR/installed/fbthrift/bin/thrift1 -r --gen py -I $SCRATCH_DIR/repos/github.com-facebook-fboss.git -I $SCRATCH_DIR/repos/github.com-facebook-fbthrift.git/ $SCRATCH_DIR/repos/github.com-facebook-fboss.git/fboss/agent/if/ctrl.thrift
+   $SCRATCH_DIR/installed/fbthrift/bin/thrift1 -r --gen py -I $SCRATCH_DIR/repos/github.com-facebook-fboss.git -I $SCRATCH_DIR/repos/github.com-facebook-fbthrift.git/ $SCRATCH_DIR/repos/github.com-facebook-fboss.git/fboss/agent/if/hw_ctrl.thrift
+   $SCRATCH_DIR/installed/fbthrift/bin/thrift1 -r --gen py -I $SCRATCH_DIR/repos/github.com-facebook-fboss.git -I $SCRATCH_DIR/repos/github.com-facebook-fbthrift.git/ $SCRATCH_DIR/repos/github.com-facebook-fboss.git/fboss/qsfp_service/if/qsfp.thrift
    mkdir -p $fboss_output_dir/lib/fb-py-libs
    cp -rf gen-py $fboss_output_dir/lib/fb-py-libs/
    cp -rf $SCRATCH_DIR/installed/fbthrift/lib/fb-py-libs/thrift_py/thrift/ $fboss_output_dir/lib/fb-py-libs/
