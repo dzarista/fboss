ddcb92aebde1a384a4ead3f9a8bb3f0411a8b049
Matthew Parker <mparker@arista.com>
2024-09-19 17:57:18 +0000

    arguments to switch-to-bmc.sh

--- arista/core/scripts/switch-to-bmc.sh
+++ arista/core/scripts/switch-to-bmc.sh
@@ -2,6 +2,21 @@
 
 # Switch the system to BMC mode.
 
+IGNORE_OUT="2>/dev/null > /dev/null"
+USB_PATH="/sys/bus/usb/devices/usb1/1-2"
+
+no_reboot=0
+check_usb_connectivity=0
+
+usage() {
+   program=$(basename "$0")
+   echo "Usage:"
+   echo "$program [--no-reboot|--check-usb-connectivity]"
+   echo "      --no-reboot : Set next boot to BMC but do not reboot"
+   echo "      --check-usb-connectivity : Check USB connection to BMC"
+   exit 1
+}
+
 switch_to_bmc_fairywren() {
    REV=$1
    DEV_ADDR=0x52
@@ -24,13 +39,98 @@ switch_to_bmc_fairywren() {
    sleep 0.1
    i2cset -f -y 1 "$DEV_ADDR" 0xf4 0x1
    sleep 0.1
-   echo 0xdead > /run/devmap/fpgas/MERU_SCM_CPLD/chassis_power_cycle
+   if [ $no_reboot -eq 0 ]; then
+      echo 0xdead > /run/devmap/fpgas/MERU_SCM_CPLD/chassis_power_cycle
+   fi
 }
+
+wait_for_cmd() {
+   cmd="$1"
+   timeout="$2"
+   while [ $timeout -gt 0 ]; do
+      if eval "$cmd"; then
+         return 0
+      fi
+      sleep 5
+      timeout=$(( $timeout - 5 ))
+   done
+   return 1
+}
+
+re_enumerate_usb0() {
+   echo "Attempting to re-enumerate usb0"
+   for v in $(seq 0 1); do
+      echo "$v" > "$USB_PATH/authorized"
+      wait_for_cmd "cat $USB_PATH/authorized | grep $v"
+      sleep 1
+   done
+   return 0
+}
+
+check_usb_connectivity_fairywren() {
+   MAX_RETRIES=3
+   bmc_not_reset="/run/devmap/fpgas/MERU_SCM_CPLD/bmc_not_reset"
+   for i in $(seq 1 $MAX_RETRIES); do
+      echo "Turning off BMC"
+      echo 0 > "$bmc_not_reset"
+      sleep 0.25
+      echo "Turning on BMC"
+      echo 1 > "$bmc_not_reset"
+      sleep 0.25
+
+      for retries in $(seq 1 3); do
+         echo "Waiting for BMC usb Hub to show up..."
+         if ! wait_for_cmd "ls $USB_PATH/ $IGNORE_OUT" 300; then
+            break
+         fi
+         echo "Waiting for BMC usb0 interface to show up..."
+         if wait_for_cmd "ls $USB_PATH/1-2:1.0/ $IGNORE_OUT" 150; then
+            echo "Waiting for usb ipv6 config to exist..."
+            if wait_for_cmd "ls /proc/sys/net/ipv6/conf/usb0/disable_ipv6 $IGNORE_OUT" 100; then
+               sysctl --quiet --write net.ipv6.conf.usb0.disable_ipv6=0 > /dev/null
+               sleep 0.25
+               echo "ip link set dev usb0 up"
+               ip link set dev usb0 up > /dev/null
+               sleep 0.25
+               echo "Waiting for ping6 usb0 default IP fe80::1..."
+               if ! wait_for_cmd "ping6 fe80::1%usb0 -c 1 $IGNORE_OUT" 30; then
+                  break
+               fi
+               return 0
+            fi
+         fi
+         re_enumerate_usb0
+      done
+      echo "Timed out on attempt $i"
+   done
+   echo "Timed out waiting for usb interface after $MAX_RETRIES attempts"
+   return 1
+}
+
+if [ $# -eq 1 ]; then
+   case "$1" in
+      "--no-reboot")
+         no_reboot=1
+         ;;
+      "--check-usb-connectivity")
+         check_usb_connectivity=1
+         ;;
+      *)
+         usage
+         ;;
+   esac
+elif [ $# -gt 1 ]; then
+   usage
+fi
 
 PRODUCT=$(dmidecode -t 2 | grep "Product" | awk '{print $3}')
 VERSION=$(dmidecode -t 2 | grep "Version" | awk '{print $2}')
 if [[ "${PRODUCT}" =~ MERU800B(I|F)A ]]; then
-   switch_to_bmc_fairywren "${VERSION}"
+   if [ $check_usb_connectivity -eq 0 ]; then
+      switch_to_bmc_fairywren "${VERSION}"
+   else
+      check_usb_connectivity_fairywren
+   fi
 else
    echo "Product not supported!"
 fi
