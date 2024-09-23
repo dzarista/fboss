dzarista/fboss

commit e9ce24b3c86ee6b72c1cab07e896f4af149b9b8c

Fill cpuCosQueueId in rx packet sent to swagent
Summary: set cosQueueId in rx packet send to swagent

Reviewed By: zechengh09

Differential Revision: D59589540

fbshipit-source-id: 3594f00437ce7103e2913733723b31ba95edba79

fboss/agent/Utils.cpp
@@ -1009,4 +1009,23 @@ bool haveParallelLinksToInterfaceNodes(
   return false;
 };
 
+CpuCosQueueId hwQueueIdToCpuCosQueueId(uint8_t hwQueueId) {
+  switch (hwQueueId) {
+    case 0:
+      return CpuCosQueueId::LOPRI;
+    case 1:
+      return CpuCosQueueId::DEFAULT;
+    case 2:
+      return CpuCosQueueId::MIDPRI;
+    /* On asics with 8 queues, cosQueue 7 is high priority
+     * bcm has 10 mcast cpu queues and use queue 9 as high priority queue
+     */
+    case 7:
+    case 9:
+      return CpuCosQueueId::HIPRI;
+    default:
+      XLOG(FATAL) << "Got Invalid hwQueueId " << hwQueueId;
+      break;
+  }
+}
 } // namespace facebook::fboss

fboss/agent/Utils.h
@@ -439,4 +439,5 @@ bool haveParallelLinksToInterfaceNodes(
         switchNameToSwitchIds,
     SwitchIdScopeResolver& scopeResolver);
 
+CpuCosQueueId hwQueueIdToCpuCosQueueId(uint8_t hwQueueId);
 } // namespace facebook::fboss

fboss/agent/mnpu/SplitAgentThriftSyncer.cpp
@@ -76,6 +76,9 @@ void SplitAgentThriftSyncer::packetReceived(
   if (pkt->getSrcAggregatePort()) {
     rxPkt.aggPort() = pkt->getSrcAggregatePort();
   }
+  if (pkt->cosQueue()) {
+    rxPkt.cosQueue() = hwQueueIdToCpuCosQueueId(*pkt->cosQueue());
+  }
   // coalesce the IOBuf before copy
   pkt->buf()->coalesce();
   rxPkt.data() = IOBuf::copyBuffer(pkt->buf()->data(), pkt->buf()->length());

commit a305fd020ab06dd6229a5e8fbd7700cbc12890ed

ARISTA HACK to fix fboss_hw_agent crash on Viper.
Return queue Id even if it doesn't map to the right priority (META is
going to fix the helper to map the right queue IDs on DNX).

The issue was introduced in e9ce24b3c86ee6b72c1cab07e896f4af149b9b8c
upstream.

fboss/agent/Utils.cpp
@@ -1024,8 +1024,10 @@ CpuCosQueueId hwQueueIdToCpuCosQueueId(uint8_t hwQueueId) {
     case 9:
       return CpuCosQueueId::HIPRI;
     default:
-      XLOG(FATAL) << "Got Invalid hwQueueId " << hwQueueId;
-      break;
+      // ARISTA Hack to avoid fboss_hw_agent crash on J3.
+      //XLOG(FATAL) << "Got Invalid hwQueueId " << hwQueueId;
+      //break;
+      return CpuCosQueueId::DEFAULT;
   }
 }
 } // namespace facebook::fboss