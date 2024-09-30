ce5da637f422ff6efdc2115d34cceb3225cecc70
dzarista <daniel.zhu@arista.com>
2024-09-30 09:36:54 -0700

    interim

--- arista/showtech/CfmShowtech.cpp
+++ arista/showtech/CfmShowtech.cpp
@@ -3,24 +3,25 @@
 
 #include "CfmShowtech.h"
 #include <filesystem>
-#include <iostream>
 #include <fstream>
-#include <vector>
+#include <iostream>
 #include <numeric>
+#include <vector>
 
 namespace cfm {
 
-double forecast( double x, const std::vector< double > & x_values,
-                 const std::vector< double > & y_values ) {
+double forecast( double x, const std::array< double, ARRAY_SIZE > & x_values,
+                const std::array< double, ARRAY_SIZE > & y_values ) {
     size_t n = x_values.size();
 
     double sum_x = std::accumulate( x_values.begin(), x_values.end(), 0.0 );
     double sum_y = std::accumulate( y_values.begin(), y_values.end(), 0.0 );
-    double sum_x2 = std::accumulate(
-          x_values.begin(), x_values.end(), 0.0,
-          []( double acc, double xi ) { return acc + xi * xi; } );
-    double sum_xy = std::inner_product(
-          x_values.begin(), x_values.end(), y_values.begin(), 0.0 );
+
+    double sum_x2 = std::accumulate( x_values.begin(), x_values.end(), 0.0,
+                                    []( double acc, double xi ) { return acc + xi * xi; } );
+
+    double sum_xy = std::inner_product( x_values.begin(), x_values.end(),
+                                       y_values.begin(), 0.0 );
 
     double m = ( n * sum_xy - sum_x * sum_y ) / ( n * sum_x2 - sum_x * sum_x );
     double b = ( sum_y - m * sum_x ) / n;
@@ -28,74 +29,93 @@ double forecast( double x, const std::vector< double > & x_values,
     return m * x + b;
 }
 
-int readIntFromFile( const std::string & filePath ) {
-    std::ifstream file( filePath );
+int readIntFromFile(const std::string &filePath) {
+  std::ifstream file(filePath);
 
-    if ( !file.is_open() ) {
-        std::cerr << "Error: Could not open file " << filePath << std::endl;
-        return -1;
-    }
+  if (!file.is_open()) {
+    std::cerr << "Error: Could not open file " << filePath << std::endl;
+    return -1;
+  }
 
-    int number;
-    file >> number;
-    return number;
+  int number;
+  file >> number;
+  return number;
 }
 
 int getPsuCount() {
-   int psuCount = 0;
-   for ( const auto &entry : std::filesystem::directory_iterator( sensorPath ) ) {
-      std::string fileName = entry.path().filename().string();
-      if ( fileName.find( "PSU" ) == 0 ) {
-         psuCount++;
-      }
-   }
-   return psuCount;
+  int psuCount = 0;
+  for (const auto &entry : std::filesystem::directory_iterator(sensorPath)) {
+    std::string fileName = entry.path().filename().string();
+    if (fileName.find("PSU") == 0) {
+      psuCount++;
+    }
+  }
+  return psuCount;
 }
 
 int getFanCount() {
-   int fanCount = 0;
-   for ( const auto &entry : std::filesystem::directory_iterator( sensorPath ) ) {
-      std::string fileName = entry.path().filename().string();
-      if ( fileName.find( "FAN_CPLD" ) == 0 ) {
-         fanCount++;
-      }
-   }
-   return fanCount;
+  int fanCount = 0;
+  for (const auto &entry : std::filesystem::directory_iterator(sensorPath)) {
+    std::string fileName = entry.path().filename().string();
+    if (fileName.find("FAN_CPLD") == 0) {
+      fanCount++;
+    }
+  }
+  return fanCount;
 }
-
 
 int getFanId() {
-   if ( getFanCount() == 1 ) {
-      return readIntFromFile( sensorPath + "/FAN_CPLD/fan1_id" );
-   }
-   else {
-      return readIntFromFile( sensorPath + "/FAN_CPLD0/fan1_id" );
-   }
+  if (getFanCount() == 1) {
+    return readIntFromFile(sensorPath + "/FAN_CPLD/fan1_id");
+  } else {
+    return readIntFromFile(sensorPath + "/FAN_CPLD0/fan1_id");
+  }
 }
 
-int getFanPwm() {
+int getFanPwm(const std::string &filePath) {
+  return readIntFromFile(filePath + "/pwm1");
+}
+
+int getPsuRpm( const std::string &filePath ) {
    return 0;
 }
 
-int getPsuPwm() {
-   return 0;
+double getPsuPwm( const std::string &filePath ) {
+   int rpm = getPsuRpm( filePath );
+   // SanyoDenki
+  if ( getFanId() == 0 ) {
+     if ( getFanCount() == 1 ) {
+        return forecast( rpm, VIPER_SD_PSU_RPM_LINE, PWM_LINE );
+     }
+     else {
+        return forecast( rpm, WHISTLER_SD_PSU_RPM_LINE, PWM_LINE );
+     }
+  }
+
+  //  Delta
+  else if ( getFanId() == 1 ) {
+     if ( getFanCount() == 1 ) {
+        return forecast( rpm, VIPER_DELTA_PSU_RPM_LINE, PWM_LINE );
+     }
+     else {
+        return forecast( rpm, WHISTLER_DELTA_PSU_RPM_LINE, PWM_LINE );
+     }
+  }
+  return -1;
 }
 
-double calcPsuCfm() {
-   return 0;
-}
+double calcPsuCfm() { return 0; }
 
 double calcFanCfm() {
-   if ( getFanCount() == 1 ) {
-      double fanPwmPercent = ( static_cast< double >( getFanPwm() ) / MAX_PWM ) * 100.0;
-   }
-   return 0;
+  if (getFanCount() == 1) {
+     double fanPwmPercent = ( static_cast< double >( getFanPwm( sensorPath + "/FAN_CPLD" ) ) / MAX_PWM ) * 100.0;
+  }
+  return 0;
 }
 
 void printCfmInfo() {
-   double totalCfm = calcFanCfm() + calcPsuCfm();
-   std::cout << "TOTAL CFM: " << totalCfm <<  std::endl;
+  double totalCfm = calcFanCfm() + calcPsuCfm();
+  std::cout << "TOTAL CFM: " << totalCfm << std::endl;
 }
 
-} //namespace cfm
-
+} // namespace cfm
--- arista/showtech/CfmShowtech.h
+++ arista/showtech/CfmShowtech.h
@@ -4,12 +4,47 @@
 #ifndef SHOWTECH_CFMSHOWTECH_H
 #define SHOWTECH_CFMSHOWTECH_H
 
+#include <array>
 #include <string>
 
 namespace cfm {
 
 inline const std::string sensorPath = "/run/devmap/sensors";
 const int MAX_PWM = 255;
+const size_t ARRAY_SIZE = 11;
 
-} //namespace cfm
+const std::array<double, ARRAY_SIZE> PWM_LINE = {100, 90, 80, 70, 60, 50,
+                                                 40,  30, 20, 10, 0};
+const std::array<double, ARRAY_SIZE> VIPER_SD_FAN_LINE = {
+    353.22, 329.06, 304.72, 271.82, 243, 211.46,
+    178.04, 147.72, 117.4,  77,     57.6};
+const std::array<double, ARRAY_SIZE> VIPER_PSU_CFM_LINE = {
+    19.39, 18.97, 16.14, 14.09, 12, 9.77, 7.48, 5.64, 4.8, 3.5, 2.2};
+const std::array<double, ARRAY_SIZE> VIPER_SD_PSU_RPM_LINE = {
+    23500, 23150, 20700, 18100, 15550, 12970, 10352, 7768, 6750, 6750, 6750};
+const std::array<double, ARRAY_SIZE> VIPER_DELTA_PSU_RPM_LINE = {
+    23584, 23248, 20688, 18176, 15520, 12976, 10367, 7740, 6916, 7816, 7500};
+const std::array<double, ARRAY_SIZE> VIPER_DELTA_FAN_LINE = {
+    377.82, 355.36, 323.22, 290.72, 255.6, 221.66,
+    189.34, 151.62, 117.1,  87.2,   42.8};
+const std::array<double, ARRAY_SIZE> WHISTLER_SD_FAN_LINE_1TO4 = {
+    313.3, 293.9, 264.7, 241.2, 212.8, 183.2, 155.6, 131.2, 101.3, 76.4, 76.4};
+const std::array<double, ARRAY_SIZE> WHISTLER_SD_FAN_LINE_5TO12 = {
+    502.5, 469.7, 425.8, 382.2,  337.7, 292.6,
+    249.2, 207.5, 161.3, 120.31, 120.31};
+const std::array<double, ARRAY_SIZE> WHISTLER_SD_PSU_CFM_LINE = {
+    71.8, 67.2, 61.6, 56, 50.3, 45.6, 39.9, 35.3, 29.5, 29.5, 29.5};
+const std::array<double, ARRAY_SIZE> WHISTLER_SD_PSU_RPM_LINE = {
+    23500, 23300, 20800, 18100, 15550, 12910, 10360, 7750, 6200, 6175, 6200};
+const std::array<double, ARRAY_SIZE> WHISTLER_DELTA_FAN_LINE_1TO4 = {
+    330.1, 315.2, 279.9, 253.7, 224.5, 192.7, 165.3, 137.9, 106.8, 76.4, 76.1};
+const std::array<double, ARRAY_SIZE> WHISTLER_DELTA_FAN_LINE_5TO12 = {
+    529.5, 503.7, 450.2, 401.9, 356.3, 307.7,
+    264.8, 218.1, 170.1, 120.4, 119.8};
+const std::array<double, ARRAY_SIZE> WHISTLER_DELTA_PSU_CFM_LINE = {
+    75.7, 72.1, 65.1, 58.9, 53.1, 48.0, 42.4, 37.1, 31.1, 29.5, 29.4};
+const std::array<double, ARRAY_SIZE> WHISTLER_DELTA_PSU_RPM_LINE = {
+    23552, 23104, 20680, 18152, 15600, 12936, 10376, 7764, 6360, 6390, 6390};
+
+} // namespace cfm
 #endif // SHOWTECH_CFMSHOWTECH_H
