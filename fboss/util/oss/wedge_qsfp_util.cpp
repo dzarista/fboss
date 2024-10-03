6692eaaf8f127b85e745fb6651f158fd412ed58c
aalamsi22 <alamsi@arista.com>
2024-10-03 23:10:29 +0000

    updated wedge_qsfp_util meru800biab reference

--- fboss/util/oss/wedge_qsfp_util.cpp
+++ fboss/util/oss/wedge_qsfp_util.cpp
@@ -58,7 +58,7 @@ std::pair<std::unique_ptr<TransceiverI2CApi>, int> getTransceiverAPI() {
               .get();
       auto ioBus = std::make_unique<BspIOBus>(systemContainer);
       return std::make_pair(std::move(ioBus), 0);
-    } else if (FLAGS_platform == "meru800bia") {
+    } else if (FLAGS_platform == "meru800bia" || FLAGS_platform == "meru800biab") {
       auto systemContainer =
           BspGenericSystemContainer<Meru800biaBspPlatformMapping>::getInstance()
               .get();
