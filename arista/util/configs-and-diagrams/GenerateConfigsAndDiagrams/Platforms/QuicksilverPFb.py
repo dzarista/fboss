289e584a3b8ea6ea1509d92bd804c2b119ddfdbb
aalamsi22 <alamsi@arista.com>
2025-02-01 02:16:15 +0000

    meru800ba: Generate basic SMB configs

--- arista/util/configs-and-diagrams/GenerateConfigsAndDiagrams/Platforms/QuicksilverPFb.py
+++ arista/util/configs-and-diagrams/GenerateConfigsAndDiagrams/Platforms/QuicksilverPFb.py
@@ -2,14 +2,51 @@
 # Arista Networks, Inc. Confidential and Proprietary.
 
 from ..BaseConfigs import (
+   PciDeviceConfig,
    PlatformConfig,
-   SCMFairywren
+   SCMFairywren,
+   SlotConfig,
+   SMBUnit
 )
 
 
 class QuicksilverPFbSCM( SCMFairywren ):
    def __init__( self ):
+      self.supportsP1 = False
       super().__init__()
+
+      self.addOutgoingSlotConfigs( [
+         SlotConfig(
+            slotName="SMB_SLOT@0",
+            outgoingI2cBuses=[
+               self.scmI2cMaster1.buses[ 0 ],
+               self.scmI2cMaster1.buses[ 2 ],
+               self.scmI2cMaster1.buses[ 3 ]
+            ]
+         )
+      ] )
+
+class QuicksilverPFbSMB( SMBUnit ):
+   prefixSymlink = 'MERU800BA'
+
+   def __init__( self ):
+      super().__init__( self.prefixSymlink )
+
+      self.setSlotTypeConfig(
+         numOutgoingI2cBuses=3,
+         idPromConfigBusName="INCOMING@0",
+         idPromConfigAddress="0x50",
+         idPromConfigKernelDeviceName="24c512",
+         idPromConfigOffset=15360
+      )
+
+      self.addPciDeviceConfigs( [
+         PciDeviceConfig( "SMB_FPGA", "0x3475", "0x0001", "0x3475", "0x0009",
+                         symlinkDeviceName="MERU800BA_SMB_FPGA" )
+      ] )
+
+      smbFpga = self.pciDeviceConfigs[ 0 ]
+      smbFpga.addInfoRomConfigs( "0x100" )
 
 
 class QuicksilverPFb( PlatformConfig ):
@@ -20,6 +57,7 @@ class QuicksilverPFb( PlatformConfig ):
 
       self.addPmUnitConfigs( [
          QuicksilverPFbSCM(),
+         QuicksilverPFbSMB()
       ] )
 
       self.addI2cAdaptersFromCpu( [ "SMBus I801 adapter at 1000" ] )
