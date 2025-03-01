e91fecb288567e5cd02f3239e8c499581dd1f133
Matthew Parker <mparker@arista.com>
2025-03-01 00:00:42 +0000

    merge

--- arista/util/configs-and-diagrams/GenerateConfigsAndDiagrams/Platforms/QuicksilverPFb.py
+++ arista/util/configs-and-diagrams/GenerateConfigsAndDiagrams/Platforms/QuicksilverPFb.py
@@ -2,6 +2,7 @@
 # Arista Networks, Inc. Confidential and Proprietary.
 
 from ..BaseConfigs import (
+   FANCpld,
    PciDeviceConfig,
    PlatformConfig,
    SCMFairywren,
@@ -39,6 +40,9 @@ class QuicksilverPFbSMB( SMBUnit ):
    def __init__( self ):
       super().__init__( self.prefixSymlink )
 
+      smbFanCpld = FANCpld( "0x60", "fan_cpld", "FAN_CPLD", incomingBusIndex=2 )
+      smbFanCpld.addFANRpms( 4, upperCriticalVal=14900.0, lowerCriticalVal=1100.0 )
+
       self.setSlotTypeConfig(
          numOutgoingI2cBuses=3,
          idPromConfigBusName="INCOMING@0",
@@ -68,6 +72,10 @@ class QuicksilverPFbSMB( SMBUnit ):
          smbIsl0V75,
          smbIslOpticsA,
          smbIslOpticsB
+      ] )
+
+      self.addI2cDeviceConfigs( [
+         smbFanCpld
       ] )
 
       self.addPciDeviceConfigs( [
