d27f2b679511525194f152a973aa0955a737d625
srv-fboss-arista-robot <srv-fboss-arista@arista.com>
2025-04-09 12:11:51 +0000

    Squashed 'fboss.bsp.arista/' changes from 8bc5c6d5cb..19dc32fb97
    19dc32fb97 Merge pull request #98 from dzarista/showtech_improvements
    720d5530c1 Showtech improvements: Limit length, add service logs, stop fboss2 commands in ramdisk, better error handling
    
    git-subtree-dir: fboss.bsp.arista
    git-subtree-split: 19dc32fb9744d2ae99b09323ccdfa2c89672c451

--- showtech/ShowtechUtils.h
+++ showtech/ShowtechUtils.h
@@ -9,6 +9,7 @@
 namespace showtech {
 int run_cmd(std::string cmd, std::string &output);
 std::string run_cmd_no_check(std::string cmd);
+std::string run_cmd_with_limit(std::string cmd, int max_lines = 5000);
 void print_fboss2_show_cmd(std::string cmd);
 void strip(std::string &str);
 int get_max_i2c_bus();
