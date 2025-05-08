e9b52f5cab6150dee6770410b254449de0924125
aalamsi22 <alamsi@arista.com>
2025-05-08 17:40:20 +0000

    sync setup.py

--- fboss/oss/scripts/run_scripts/setup.py
+++ fboss/oss/scripts/run_scripts/setup.py
@@ -29,18 +29,27 @@ class SetupFboss:
 
     USER_BDE = "linux-user-bde"
     KERNEL_BDE = "linux-kernel-bde"
+    KERNEL_NGBDE = "linux_ngbde"
+
     USER_BDE_KO = USER_BDE + ".ko"
     KERNEL_BDE_KO = KERNEL_BDE + ".ko"
+    KERNEL_NGBDE_KO = KERNEL_NGBDE + ".ko"
+
     KMOD_FULL_PATH = os.path.join("/lib/modules/" + platform.uname().release)
     USER_BDE_KO_FULL_PATH = os.path.join(KMOD_FULL_PATH, USER_BDE_KO)
     KERNEL_BDE_KO_FULL_PATH = os.path.join(KMOD_FULL_PATH, KERNEL_BDE_KO)
+    KERNEL_NGBDE_KO_FULL_PATH = os.path.join(KMOD_FULL_PATH, KERNEL_NGBDE_KO)
 
     # Unfortunately, lsmod prints _ (underscore) for - (dash)
     LSMOD_USER_BDE = "linux_user_bde"
     LSMOD_KERNEL_BDE = "linux_kernel_bde"
+    LSMOD_KERNEL_NGBDE = "linux_ngbde"
 
     SRC_USER_BDE_KO_FULL_PATH = os.path.join(os.environ["FBOSS_KMODS"], USER_BDE_KO)
     SRC_KERNEL_BDE_KO_FULL_PATH = os.path.join(os.environ["FBOSS_KMODS"], KERNEL_BDE_KO)
+    SRC_KERNEL_NGBDE_KO_FULL_PATH = os.path.join(os.environ["FBOSS_KMODS"], KERNEL_NGBDE_KO)
+
+    useNgbde = False
 
     TH = "th"
     TH3 = "th3"
@@ -49,6 +58,7 @@ class SetupFboss:
     J3 = "j3"
     J3B = "j3b"
     R3 = "r3"
+    TH5 = "th5"
     ### ARISTA END ###
 
     def __init__(self):
@@ -101,29 +111,38 @@ class SetupFboss:
             )
         elif [x for x in output if "Broadcom" in x and "8900" in x]:
             self.src_fruid_full_path = os.path.join(
-                *[os.environ["FBOSS_DATA"], SetupFboss.J3, SetupFboss.FRUID_CONF]
+                *[os.environ["FBOSS_DATA"], SetupFboss.TH5, SetupFboss.FRUID_CONF]
             )
             self.src_bde_full_path = os.path.join(
-                *[os.environ["FBOSS_DATA"], SetupFboss.J3, SetupFboss.BDE_CONF]
+                *[os.environ["FBOSS_DATA"], SetupFboss.TH5, SetupFboss.BDE_CONF]
             )
     ### ARISTA END ###
 
+        self.useNgbde = "linux_ngbde" in open(self.src_bde_full_path).read()
 
     def _cleanup_old_setup(self):
         if os.path.exists(SetupFboss.FRUID_FULL_PATH):
-            os.remove(SetupFboss.FRUID_FULL_PATH)
+            ### ARISTA START ###
+            pass # os.remove(SetupFboss.FRUID_FULL_PATH)
+            ### ARISTA END ###
 
         if os.path.exists(SetupFboss.BDE_CONF_FULL_PATH):
             os.remove(SetupFboss.BDE_CONF_FULL_PATH)
 
-        subprocess.run(["modprobe", "-r", SetupFboss.USER_BDE])
-        subprocess.run(["modprobe", "-r", SetupFboss.KERNEL_BDE])
+        if self.useNgbde:
+            subprocess.run(["modprobe", "-r", SetupFboss.KERNEL_NGBDE])
+        else:
+            subprocess.run(["modprobe", "-r", SetupFboss.KERNEL_BDE])
+            subprocess.run(["modprobe", "-r", SetupFboss.USER_BDE])
 
         if os.path.exists(SetupFboss.USER_BDE_KO_FULL_PATH):
             os.remove(SetupFboss.USER_BDE_KO_FULL_PATH)
 
         if os.path.exists(SetupFboss.KERNEL_BDE_KO_FULL_PATH):
             os.remove(SetupFboss.KERNEL_BDE_KO_FULL_PATH)
+
+        if os.path.exists(SetupFboss.KERNEL_NGBDE_KO_FULL_PATH):
+            os.remove(SetupFboss.KERNEL_NGBDE_KO_FULL_PATH)
 
     def _copy_configs(self):
         if not os.path.exists(SetupFboss.FRUID_FULL_PATH):
