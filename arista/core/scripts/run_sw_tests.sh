91e8e6b0bc0be2cce3dda127ba3a576693d7c456
sauravg-arista <sgyawali@arista.com>
2025-06-30 16:37:50 -0400

    Use gtest filter to exclude test cases in swtests (#1063)
    * Use gtest_filter to exclude test cases for swtests
    * Remove swtest artifacts from core rpm

--- arista/core/scripts/run_sw_tests.sh
+++ arista/core/scripts/run_sw_tests.sh
@@ -1,55 +1,34 @@
-echo "Running sw_tests - requires a prior successful build if using fbossctl"
+echo "Running sw_tests - requires a prior successful build"
+echo "This script needs to be run from arista-fboss root"
 
-TEST_PATH_REGEX=".*tests?$"
-HW_TEST_PATH_REGEX=".*hw_tests?$"
+# Add failing btests we should ignore to this list
+EXCLUDE_LIST=(
+    # "UtilsTest.ParseDevicePath"
+)
+gtest_filter=-$(IFS=:; echo "${EXCLUDE_LIST[*]}")
+
+test_regex=".*tests?$"
+hwtest_regex=".*hw_tests?$"
 
 passed=()
 failed=()
-skipped=()
-
-# Add failing btests we should ignore to this list
-EXCLUDE_LIST=()
 
 # Default to fboss build environment if no arg provided
 BUILD_DIR="${1:-/var/FBOSS/tmp_build_dir/build/fboss}"
 
-if [ -d "$BUILD_DIR" ]; then
-    cd "$BUILD_DIR"
-else
+if [ ! -d "$BUILD_DIR" ]; then
     echo "$BUILD_DIR doesn't exist. A prior successful build is required if using fbossctl."
     echo "If not using fbossctl, make sure you are correctly passing in the directory where the binaries are located"
     exit 1
 fi
 
-for file in *; do
-    EXCLUDED=false
-    for excluded_file in "${EXCLUDE_LIST[@]}"; do
-        if [[ "$file" == "$excluded_file" ]]; then
-            EXCLUDED=true
-            break
-        fi
-    done
-
-    if $EXCLUDED; then
-        skipped+=("$file")
-        continue
-    fi
-
-    if [[ -x "$file" && "$file" =~ $TEST_PATH_REGEX && ! "$file" =~ $HW_TEST_PATH_REGEX ]]; then
-        if [[ "$file" == "fan_service_sw_test" && "$BUILD_DIR" = "/var/FBOSS/tmp_build_dir/build/fboss" ]]; then
-            # Must be run in a certain directory relative to the fan_service.json file
-            cp -f fan_service_sw_test /var/FBOSS/fboss.git
-            cd "/var/FBOSS/fboss.git" && "./fan_service_sw_test"
-            cd "$BUILD_DIR"
-        else
-            ./"$file"
-        fi
-
-        if [[ $? -eq 0 ]]; then
-            passed+=("$file")
-        else
-            failed+=("$file")
-        fi
+tests=$(find $BUILD_DIR -type f -executable -regex $test_regex -not -regex $hwtest_regex)
+for test in $tests; do
+    $test --gtest_filter=$gtest_filter
+    if [[ $? -eq 0 ]]; then
+        passed+=($(basename "$test"))
+    else
+        failed+=($(basename "$test"))
     fi
 done
 
@@ -58,29 +37,20 @@ echo "Summary:"
 echo "---------"
 if [[ ${#passed[@]} -gt 0 ]]; then
     echo "Passed:"
-    for p in "${passed[@]}"; do
-        echo "  $p"
-    done
+    echo "${passed[*]}"
 else
     echo "No scripts passed."
 fi
 
-if [[ ${#skipped[@]} -gt 0 ]]; then
+if [[ ${#EXCLUDE_LIST[@]} -gt 0 ]]; then
     echo "Skipped:"
-    for f in "${skipped[@]}"; do
-        echo "  $f"
-    done
-else
-    echo "No scripts skipped."
+    echo "${EXCLUDE_LIST[*]}"
 fi
 
 if [[ ${#failed[@]} -gt 0 ]]; then
     echo "Failed:"
-    for f in "${failed[@]}"; do
-        echo "  $f"
-    done
+    echo "${failed[*]}"
     exit 1
 else
     echo "No scripts failed."
-    exit 0
 fi
