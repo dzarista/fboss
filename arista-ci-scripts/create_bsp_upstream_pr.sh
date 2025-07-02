1cd0d8d9ef0a399f4f880c12a5d0bd8f9f7c49b9
Anoop Kailathu Rajeev <arajeev@arista.com>
2025-07-02 04:39:23 +0000

    addressed review comment

--- arista-ci-scripts/create_bsp_upstream_pr.sh
+++ arista-ci-scripts/create_bsp_upstream_pr.sh
@@ -16,18 +16,20 @@ email_subject_file=status_email_subject.txt
 
 echo "fboss.bsp.arista pull request from $PR_BRANCH" > $email_subject_file
 
+SCRIPT_START_DIR=$(pwd)
+
 mkdir upstream_repo
 cd upstream_repo
-GIT_SSH_COMMAND="ssh -i ../private.key -o IdentitiesOnly=yes" git clone $repo_name
+GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git clone $repo_name
 cd fboss.bsp.arista
-if GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git ls-remote --exit-code --heads $repo_name $upstream_pr_branch_name; then
-   GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git push origin -d $upstream_pr_branch_name
+if GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git ls-remote --exit-code --heads $repo_name $upstream_pr_branch_name; then
+   GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git push origin -d $upstream_pr_branch_name
 fi
-GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git checkout -b $upstream_pr_branch_name origin/main || exit 1
-cd ..
+GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git checkout -b $upstream_pr_branch_name origin/main || exit 1
+cd "${SCRIPT_START_DIR}/upstream_repo"
 # Real text diff is found in text_only.patch. They can be applied in upstream_repo with 'patch' command
-if patch -p1 < ../text_only.patch &> patch_status.txt; then
-   cd ..
+if patch -p1 < ${SCRIPT_START_DIR}/text_only.patch &> patch_status.txt; then
+   cd "${SCRIPT_START_DIR}"
    # Binary file diff is found in binary_only.diff. 'patch' command doesn't work in the case of binary files.
    # Hence, process them one after the other.
    while read -r old_file modified_file; do
@@ -45,16 +47,16 @@ if patch -p1 < ../text_only.patch &> patch_status.txt; then
    cd upstream_repo/fboss.bsp.arista
    git config --global user.name "srv-fboss-arista"
    git config --global user.email "srv-fboss-arista@arista.com"
-   GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git add -A
-   GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git commit -m "upstreaming BSP changes"
-   GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git push origin $upstream_pr_branch_name
-   GIT_SSH_COMMAND="ssh -i ../../private.key -o IdentitiesOnly=yes" git checkout main || exit 1
+   GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git add -A
+   GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git commit -m "upstreaming BSP changes"
+   GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git push origin $upstream_pr_branch_name
+   GIT_SSH_COMMAND="ssh -i ${SCRIPT_START_DIR}/private.key -o IdentitiesOnly=yes" git checkout main || exit 1
    pr_link=$(gh pr create --title "$pr_title" --body "$pr_description" --head $upstream_pr_branch_name --base main --repo $repo_name --draft | grep https)
-   cd ../..
+   cd "${SCRIPT_START_DIR}"
    echo "Created a pull request from branch $upstream_pr_branch_name." > $output_file
    echo "Created the draft pull request $pr_link with all the changes in BSP subtree. Make sure the pull request matches with the changes to the subtree. Please publish the pull request after updating the title and description." > $status_email_file
 else
-   cd ..
+   cd "${SCRIPT_START_DIR}"
    echo "Couldn't create a pull request due to merge conflicts." > $status_email_file
    echo "Details of the merge conflicts" >> $output_file
    echo "==============================" >> $output_file
