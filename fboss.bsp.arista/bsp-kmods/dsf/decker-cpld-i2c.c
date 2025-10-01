b692b6171009b3f38456a19f05bd18f9043a6add
Adam Calabrigo <adamc@arista.com>
2025-10-01 10:20:50 +0000

    [drivers] create common SMB CPLD library

--- fboss.bsp.arista/bsp-kmods/dsf/decker-cpld-i2c.c
+++ fboss.bsp.arista/bsp-kmods/dsf/decker-cpld-i2c.c
@@ -20,100 +20,18 @@
 #include <linux/version.h>
 
 #include "regbit-sysfs.h"
+#include "smb-cpld-i2c.h"
 
-#define CPLD_REG_REV_MINOR	0x0
-#define CPLD_REG_REV_MAJOR	0x1
-#define CPLD_REG_CTRL_STS	0x5
-#define CPLD_REG_SYS_STS	0x8
-#define CPLD_REG_SCD_FPGA_STA	0xA
-#define CPLD_REG_JTAG_SEL	0xC
-#define CPLD_REG_IDPROM_WP	0x22
 #define CPLD_SCD0_CTRL_STA	0xA0
 #define CPLD_SCD1_CTRL_STA	0xA1
 #define CPLD_SCD2_CTRL_STA	0xA2
 #define CPLD_SCD3_CTRL_STA	0xA3
 
-static const struct regbit_sysfs_config cpld_sys_attrs[] = {
-	/*
-	 * CPLD version/sub_version @ address/offset 0x0 and 0x1.
-	 */
-	{
-		.name = "cpld_sub_ver",
-		.mode = REGBIT_FMODE_RO,
-		.reg_addr = CPLD_REG_REV_MINOR,
-		.bit_offset = 0,
-		.num_bits = 8,
-	},
-	{
-		.name = "cpld_ver",
-		.mode = REGBIT_FMODE_RO,
-		.reg_addr = CPLD_REG_REV_MAJOR,
-		.bit_offset = 0,
-		.num_bits = 8,
-	},
-
-	/*
-	 * Power Ctrl/Stat register @ address/offset 0x5.
-	 */
-	{
-		.name = "switch_card_pwr_status",
-		.mode = REGBIT_FMODE_RO,
-		.reg_addr = CPLD_REG_CTRL_STS,
-		.bit_offset = 0,
-		.num_bits = 1,
-		.flags = RBS_FLAG_SHOW_NOTES,
-		.help_str = "0: switch card power bad\n"
-			    "1: switch card power good",
-	},
-
+static const struct regbit_sysfs_config decker_attrs[] = {
 	/* MERU TODO: add ASIC/port status registers. */
 
 	/*
-	 * SCD_FPGA_STA @ address/offset 0xA.
-	 */
-	{
-		.name = "scd_config_done",
-		.mode = REGBIT_FMODE_RO,
-		.reg_addr = CPLD_REG_SCD_FPGA_STA,
-		.bit_offset = 0,
-		.num_bits = 1,
-		.flags = RBS_FLAG_SHOW_NOTES,
-		.help_str = "0: SCD FPGA configuration not done yet\n"
-			    "1: SCD FPGA configuration done",
-	},
-	{
-		.name = "scd_config",
-		.mode = REGBIT_FMODE_RW,
-		.reg_addr = CPLD_REG_SCD_FPGA_STA,
-		.bit_offset = 3,
-		.num_bits = 1,
-		.flags = RBS_FLAG_SHOW_NOTES,
-		.help_str = "0: normal operation. Default\n"
-			    "1: initiate SCD FPGA re-configuration",
-	},
-	{
-		.name = "scd_hold",
-		.mode = REGBIT_FMODE_RW,
-		.reg_addr = CPLD_REG_SCD_FPGA_STA,
-		.bit_offset = 4,
-		.num_bits = 1,
-		.flags = RBS_FLAG_SHOW_NOTES,
-		.help_str = "0: normal operation. Default\n"
-			    "1: hold SCD output for SCD hitless update",
-	},
-	{
-		.name = "scd_reset",
-		.mode = REGBIT_FMODE_RW,
-		.reg_addr = CPLD_REG_SCD_FPGA_STA,
-		.bit_offset = 5,
-		.num_bits = 1,
-		.flags = RBS_FLAG_SHOW_NOTES,
-		.help_str = "0: normal operation. Default\n"
-			    "1: reset SCD",
-	},
-
-	/*
-	 * JTAG @ address/offset 0xC.
+	 * JTAG selects @ address/offset 0xC.
 	 */
 	{
 		.name = "jtag_mux_sel",
@@ -142,20 +60,6 @@ static const struct regbit_sysfs_config cpld_sys_attrs[] = {
 		.flags = RBS_FLAG_SHOW_NOTES,
 		.help_str = "0: CPU -> external JTAG selected\n"
 			    "1: CPU -> CPLD JTAG selected, bits[6:0] are ignored",
-	},
-
-	/*
-	 * IDPROM Write-Protect @ address/offset 0x22.
-	 */
-	{
-		.name = "idprom_wp",
-		.mode = REGBIT_FMODE_RW,
-		.reg_addr = CPLD_REG_IDPROM_WP,
-		.bit_offset = 0,
-		.num_bits = 1,
-		.flags = RBS_FLAG_SHOW_NOTES,
-		.help_str = "0: IDPROM write-protect disabled\n"
-			    "1: IDPROM write-protect enabled (default)",
 	},
 
 	/*
@@ -255,55 +159,20 @@ static const struct regbit_sysfs_config cpld_sys_attrs[] = {
 	},
 };
 
-static ssize_t fw_ver_show(struct device *dev, struct device_attribute *attr, char *buf)
-{
-	int ret;
-	u8 major_rev, minor_rev;
-	struct i2c_client *client = to_i2c_client(dev);
-
-	ret = i2c_smbus_read_byte_data(client, CPLD_REG_REV_MAJOR);
-	if (ret < 0)
-		return ret;
-	major_rev = (u8)ret;
-
-	ret = i2c_smbus_read_byte_data(client, CPLD_REG_REV_MINOR);
-	if (ret < 0)
-		return ret;
-	minor_rev = (u8)ret;
-
-	return sprintf(buf, "%u.%u\n", major_rev, minor_rev);
-}
-
-DEVICE_ATTR(fw_ver, 0444, fw_ver_show, NULL);
-
 static int cpld_i2c_probe(struct i2c_client *client)
 {
 	int ret;
-	u8 major_rev, minor_rev;
 
-	ret = i2c_smbus_read_byte_data(client, CPLD_REG_REV_MINOR);
+	ret = smb_cpld_fw_ver_read(client, NULL, "decker");
 	if (ret < 0)
 		return ret;
-	minor_rev = (u8)ret;
 
-	ret = i2c_smbus_read_byte_data(client, CPLD_REG_REV_MAJOR);
+	ret = smb_cpld_create_fw_ver_attr(client);
 	if (ret < 0)
 		return ret;
-	major_rev = (u8)ret;
-	dev_info(&client->dev, "decker cpld revision: %02x.%02x\n",
-		 major_rev, minor_rev);
 
-	ret = sysfs_create_file(&client->dev.kobj, &dev_attr_fw_ver.attr);
-	if (ret < 0) {
-		dev_err(&client->dev,
-			"could not create %s attribute for cpld: %d",
-			dev_attr_fw_ver.attr.name,
-			ret);
-		return ret;
-	}
-
-	return regbit_sysfs_init_i2c(&client->dev, cpld_sys_attrs,
-				     ARRAY_SIZE(cpld_sys_attrs));
+	return smb_cpld_init_with_attrs(&client->dev, decker_attrs,
+				ARRAY_SIZE(decker_attrs));
 }
 
 static const struct i2c_device_id cpld_dev_ids[] = {
---
+++ fboss.bsp.arista/bsp-kmods/lib/smb-cpld-i2c.h
@@ -0,0 +1,46 @@
+/* Copyright (c) 2025 Arista Networks, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _SMB_CPLD_I2C_H_
+#define _SMB_CPLD_I2C_H_
+
+#include <linux/i2c.h>
+#include <linux/device.h>
+
+#include "regbit-sysfs.h"
+
+/* Common CPLD register definitions */
+#define CPLD_REG_REV_MINOR	0x0
+#define CPLD_REG_REV_MAJOR	0x1
+#define CPLD_REG_CTRL_STS	0x5
+#define CPLD_REG_SCD_FPGA_STA	0xA
+#define CPLD_REG_JTAG_SEL	0xC
+#define CPLD_REG_IDPROM_WP	0x22
+
+/* Common regbit_sysfs_config entries */
+extern const struct regbit_sysfs_config smb_cpld_common_attrs[];
+extern const int smb_cpld_common_attrs_count;
+
+/* Common function declarations */
+int smb_cpld_fw_ver_read(struct i2c_client *client, char *buf, const char *device_name);
+ssize_t smb_cpld_fw_ver_show(struct device *dev, struct device_attribute *attr, char *buf);
+int smb_cpld_create_fw_ver_attr(struct i2c_client *client);
+int smb_cpld_init_with_attrs(struct device *dev,
+		const struct regbit_sysfs_config *driver_attrs,
+		int driver_attrs_count);
+
+#endif /* _SMB_CPLD_I2C_H_ */
