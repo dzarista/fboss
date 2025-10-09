cb8807c120b0b4f01af687f98b6d64df7a970958
Adam Calabrigo <adamc@arista.com>
2025-10-09 13:45:54 +0000

    [fan-cpld-i2c] update year in copyright headers to 2025

--- fboss.bsp.arista/bsp-kmods/dsf/dsf-fan-cpld-i2c.c
+++ fboss.bsp.arista/bsp-kmods/dsf/dsf-fan-cpld-i2c.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023 Arista Networks, Inc.
+/* Copyright (c) 2025 Arista Networks, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
