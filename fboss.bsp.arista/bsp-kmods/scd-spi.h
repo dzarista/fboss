5ed3f2e64683adf74130333f464bfc5b7cd070d1
Huy Chu <huyc@arista.com>
2025-10-14 19:53:15 +0000

    fix checkpatch warning: use tab not space where possible

--- fboss.bsp.arista/bsp-kmods/scd-spi.h
+++ fboss.bsp.arista/bsp-kmods/scd-spi.h
@@ -27,52 +27,52 @@
 #define SPI_FIFO_SIZE 34
 
 struct scd_spi_devdata {
-  struct pci_dev *pci_dev;
-  struct spi_controller *controller;
-  struct spi_device *device;
+	struct pci_dev *pci_dev;
+	struct spi_controller *controller;
+	struct spi_device *device;
 
-  u32 csr_addr;
-  void __iomem *mmio_csr;
+	u32 csr_addr;
+	void __iomem *mmio_csr;
 
-  u32 cmd_fifo_addr;
-  u32 read_fifo_addr;
-  u32 ctrl_addr;
+	u32 cmd_fifo_addr;
+	u32 read_fifo_addr;
+	u32 ctrl_addr;
 
-  u32 fifo_size;
+	u32 fifo_size;
 };
 
 union scd_spi_cmd_fifo {
-  u32 reg;
-  struct {
-    u32 write_data : 8;
-    u32 byte2 : 4;
-    u32 record_output : 1;
-    u32 byte1 : 17;
-    u32 irq : 1;
-    u32 chip_select_end : 1;
-  } __packed;
+	u32 reg;
+	struct {
+		u32 write_data : 8;
+		u32 byte2 : 4;
+		u32 record_output : 1;
+		u32 byte1 : 17;
+		u32 irq : 1;
+		u32 chip_select_end : 1;
+	} __packed;
 };
 
 union scd_spi_read_fifo {
-  u32 reg;
-  struct {
-    u32 read_data : 8;
-    u32 byte1 : 23;
-    u32 data_valid : 1;
-  } __packed;
+	u32 reg;
+	struct {
+		u32 read_data : 8;
+		u32 byte1 : 23;
+		u32 data_valid : 1;
+	} __packed;
 };
 
 union scd_spi_ctrl {
-  u32 reg;
-  struct {
-    u32 read_fifo_count : 6;
-    u32 byte1 : 10;
-    u32 write_fifo_count : 6;
-    u32 byte2 : 7;
-    u32 read_fifo_underflow : 1;
-    u32 cmd_fifo_overflow : 1;
-    u32 spi_interrupt : 1;
-  } __packed;
+	u32 reg;
+	struct {
+		u32 read_fifo_count : 6;
+		u32 byte1 : 10;
+		u32 write_fifo_count : 6;
+		u32 byte2 : 7;
+		u32 read_fifo_underflow : 1;
+		u32 cmd_fifo_overflow : 1;
+		u32 spi_interrupt : 1;
+	} __packed;
 };
 
 #endif /* _LINUX_DRIVER_SCD_SPI_H_ */
